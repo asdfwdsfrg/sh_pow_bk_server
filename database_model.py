9f801a1d1c0f7f9f9e37c28e35fc6345f6118ae3
asdfwdsfrg <asdfwdsfrg@outlook.com>
2017-06-23 23:10:52 +0800

    完成shared_power_bank的RESTFUL Api

--- database_model.py
+++ database_model.py
@@ -1,12 +1,16 @@
 from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
+import random
 from flask import abort
 import os
+
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///shared_pow_bank.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
+
 
 
 class User(db.Model):
@@ -15,12 +19,16 @@ class User(db.Model):
     email = db.Column(db.String(30))
     balance = db.Column(db.Float)
 
+
     def __init__(self, username, passwd):
         self.username = username
         self.passwd = passwd
+        self.balance = 5
+
 
     def __repr__(self):
         return '<User %r>' % self.username
+
 
 
 class Depository(db.Model):
@@ -31,6 +39,7 @@ class Depository(db.Model):
     lat = db.Column(db.Float)
     chkcode = db.Column(db.Integer)
 
+
     def __init__(self, device_id, pow_bank_num, location, lng, lat):
         self.device_id = device_id
         self.pow_bank_num = pow_bank_num
@@ -39,9 +48,43 @@ class Depository(db.Model):
         self.lat = lat
         self.chkcode = 0
 
+
     def __repr__(self):
         return '<Depository_id %r pow_bank_num %r location %r>' \
                % self.device_id, self.pow_bank_num, self.location
 
 
+def userdict(usr):
+    return {
+        'username': usr.username,
+        'balance': usr.balance,
+    }
 
+
+def depdict(dep):
+    return {
+        'id': dep.device_id,
+        'location': dep.location,
+        'pow_bank_num': dep.pow_bank_num ,
+        'coordinate' :{
+            'lng' : dep.lng,
+            'lat' : dep.lat,
+        },
+    }
+
+
+def update_chk(dep_id):
+    result = Depository.query.filter_by(device_id=dep_id).first()
+    result.chkcode = random.randint(100000, 999999)
+    db.session.commit()
+
+
+def pay_database(user, payment):
+    user.balance = user.balance - float(payment)
+    db.session.commit()
+
+
+def register_user_to_db(username, passwd):
+    us = User(username, passwd)
+    db.session.add(us)
+    db.session.commit()
---
+++ flask_server.py
@@ -0,0 +1,101 @@
+from flask import Flask, jsonify, request
+from flask import abort
+import database_model as dm
+from database_model import db,app, User, Depository
+import os
+
+
+db.create_all()
+@app.route('/depositories', methods=['GET'])
+def get_neighbor_dep():
+    if 'username' in request.args:
+        us = User.query.get(request.args['username'])
+        if (not us):
+            message = 'user does not exist'
+            return message_handler(status=404, info=message)
+        else:
+            result = Depository.query.all()
+            js = [dm.depdict(i) for i in result]
+            return jsonify(js)
+
+
+@app.route('/depositories/check', methods=['GET'])
+def check():
+    dep = Depository.query.get(request.args['id'])
+    if dep.chkcode == request.args['chkcode']:
+        message = 'OK'
+        return message_handler(status = 200, info = message)
+    else:
+        message = 'checkcode wrong'
+        return message_handler(status = 404, info = message)
+
+
+@app.route('/depositories/<int:dep_id>', methods=['GET'])
+def get_chkcode(dep_id):
+    if 'username' in request.args:
+        us = User.query.get(request.args['username'])
+        if (not us):
+            message = 'user does not exist'
+            return message_handler(status=404, info=message)
+        else:
+            dm.update_chk(dep_id)
+            result = Depository.query.get(dep_id)
+            chkcode = {
+                'chkcode': result.chkcode
+            }
+            return jsonify(chkcode)
+
+
+@app.route('/user/login', methods=['GET'])
+def get_user():
+    if 'username' in request.args:
+        us = User.query.get(request.args['username'])
+        if(not us):
+            message = 'user does not exist'
+            return message_handler(status = 404, info = message)
+        if request.args['passwd'] == us.passwd:
+            js = dm.userdict(us)
+            return jsonify(js)
+        else:
+            message = 'Password wrong'
+            return message_handler(status = 404, info = message)
+
+
+@app.route('/user/register', methods=['GET'])
+def register_user():
+    if 'username' in request.args and 'passwd' in request.args:
+        us = User.query.get(request.args['username'])
+        if (not us):
+            dm.register_user_to_db(request.args['username'], request.args['passwd'])
+            return message_handler(status = 200,info = 'register success and we provide 5 yuan for probation')
+        else:
+            message = 'user has existed'
+            return message_handler(status = 404, info = message)
+
+
+@app.route('/user/pay')
+def pay():
+    if 'username' in request.args and 'passwd' in request.args:
+        us = User.query.get(request.args['username'])
+        if (not us):
+            message = 'user does not exist'
+            return message_handler(status=404, info=message)
+        else:
+            dm.pay_database(us, request.args['payment'])
+            us = User.query.get(us.username)
+            message = 'success and your balance is %r' %us.balance
+            return message_handler(status = 200, info = message)
+
+
+def message_handler(error=None, status = 200, info = ''):
+    message = {
+            'status': status,
+            'message': info
+    }
+    resp = jsonify(message)
+    resp.status_code = status
+    return resp
+
+
+if __name__ == '__main__':
+    app.run(host = '0.0.0.0')
